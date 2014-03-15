24a3388f827d0b62bb59dd3bf9ecca9f4ee331c8
Eli <elixdlol@gmail.com>
2014-03-15 20:28:13 +0200

    Can search! now only need to create a class to create a proper JSON file and pass it to the GUI

--- omod/src/main/java/org/openmrs/module/chartsearch/fragment/controller/ResultBoxFragmentController.java
+++ omod/src/main/java/org/openmrs/module/chartsearch/fragment/controller/ResultBoxFragmentController.java
@@ -2,13 +2,22 @@
 package org.openmrs.module.chartsearch.fragment.controller;
 
 
+import org.openmrs.module.chartsearch.ChartListItem;
 import org.openmrs.module.chartsearch.SearchAPI;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
+import java.util.ArrayList;
+
 public class ResultBoxFragmentController {
 
-	public void controller(FragmentModel fragmentModel) {		
-		fragmentModel.addAttribute("prevLocations",   SearchAPI.getResults());
+	public void controller(FragmentModel fragmentModel) {
+        SearchAPI searchAPI =SearchAPI.getInstance();
+       ArrayList<String> resultList = new ArrayList<String>();
+        for(ChartListItem item : searchAPI.getResults()){
+            resultList.add("Date: " + item.getObsDate()+ " concept Name : " + item.getConceptName()+" Value: "+ item.getValue()+
+                    " Uuid: "+ item.getUuid()+ " Highlights "+ item.getHighlights() + " Obs ID: " + item.getObsId());
+        }
+		fragmentModel.addAttribute("resultList",   resultList);
 	}
 	
 	

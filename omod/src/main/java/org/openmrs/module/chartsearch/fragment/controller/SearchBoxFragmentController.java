ff93925cb99eaf8658dac07d113678e2c186d179
Eli <elixdlol@gmail.com>
2014-03-15 20:28:13 +0200

    Can search! now only need to create a class to create a proper JSON file and pass it to the GUI

--- omod/src/main/java/org/openmrs/module/chartsearch/fragment/controller/SearchBoxFragmentController.java
+++ omod/src/main/java/org/openmrs/module/chartsearch/fragment/controller/SearchBoxFragmentController.java
@@ -7,15 +7,18 @@ package org.openmrs.module.chartsearch.fragment.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.chartsearch.ChartListItem;
 import org.openmrs.module.chartsearch.SearchAPI;
 import org.openmrs.module.chartsearch.SearchPhrase;
+import org.openmrs.module.chartsearch.solr.ChartSearchSearcher;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 import org.openmrs.ui.framework.annotation.BindParams;
 import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class SearchBoxFragmentController {
-
+    private ChartSearchSearcher searcher = getComponent(ChartSearchSearcher.class);
 
 	public void get() {
 	}
@@ -26,15 +29,43 @@ public class SearchBoxFragmentController {
 	
 	
 	public String post(PageModel model, @BindParams SearchPhrase searchPhrase ,@RequestParam("patientId") Integer patient) {
+
+        SearchAPI searchAPI =SearchAPI.getInstance();
         model.addAttribute("patientID_from_get", patient);
 		//SearchAPI.setSearchPhrase(searchPhrase);
 		//SearchAPI.search();
-		List<String> temp = new ArrayList<String>();
-		temp.add(searchPhrase.getPhrase());
-		SearchAPI.setResults(temp);
+		//List<String> temp = new ArrayList<String>();
+		//temp.add(searchPhrase.getPhrase());
+        Integer length = Integer.valueOf(10);
+        Integer start = Integer.valueOf(0);
+        List<ChartListItem> items = new ArrayList<ChartListItem>();
+
+        try {
+            items = searcher.getDocumentList(patient, searchPhrase.getPhrase(), start, length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(items == null){
+           items = new ArrayList<ChartListItem>();
+            ChartListItem itemsIsNull = new ChartListItem();
+            itemsIsNull.setConceptName("items list returned from search is null");
+            items.add(itemsIsNull);
+
+        }
+
+        searchAPI.setResults(items);
+
+
 		return "redirect:chartsearch/chartsearch.page?patientId=" + patient;
 	}
-	
+
+
+    private <T> T getComponent(Class<T> clazz) {
+        List<T> list = Context.getRegisteredComponents(clazz);
+        if (list == null || list.size() == 0)
+            throw new RuntimeException("Cannot find component of " + clazz);
+        return list.get(0);
+    }
 	
 	
 	
--- omod/src/main/java/org/openmrs/module/chartsearch/page/controller/ChartsearchPageController.java
+++ omod/src/main/java/org/openmrs/module/chartsearch/page/controller/ChartsearchPageController.java
@@ -6,8 +6,10 @@ package org.openmrs.module.chartsearch.page.controller;
 
 import org.openmrs.api.context.Context;
 import org.openmrs.module.appui.UiSessionContext;
+import org.openmrs.module.chartsearch.ChartListItem;
 import org.openmrs.module.chartsearch.SearchAPI;
 import org.openmrs.module.chartsearch.solr.ChartSearchIndexer;
+import org.openmrs.module.chartsearch.solr.ChartSearchSearcher;
 import org.openmrs.ui.framework.page.PageModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,24 +29,19 @@ public class ChartsearchPageController {
 
 
     private ChartSearchIndexer chartSearchIndexer = getComponent(ChartSearchIndexer.class);
+    private ChartSearchSearcher searcher = getComponent(ChartSearchSearcher.class);
 
     public void controller(PageModel model, UiSessionContext sessionContext, @RequestParam("patientId") Integer patient) {
         //model.addAttribute("user", sessionContext.getCurrentUser());
     	
         model.addAttribute("patientID_from_get", patient);
-        log.info("getting :" + patient);
+        log.info("getting patient ID :" + patient);
         log.info("trying to index a patient");
-        ArrayList<String> lst = new ArrayList<String>();
-        if (patient == null) {
-            lst.add("PersonID is null");
-            SearchAPI.setResults(lst);
-        }
-        else {
-            lst.add("personID is "+ patient.toString());
-            SearchAPI.setResults(lst);
-        }
+      //  ArrayList<String> lst = new ArrayList<String>();
+
         
         chartSearchIndexer.indexPatientData(patient);
+        log.info("indexed patient");
         //log.info("indexed successfully");
     }
     

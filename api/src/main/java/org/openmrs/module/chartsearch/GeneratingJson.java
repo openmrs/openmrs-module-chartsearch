75b004f252e9e7e3c87e14936bb7589806318470
Eli <elixdlol@gmail.com>
2014-03-20 02:57:28 +0200

    another fix to the json

--- api/src/main/java/org/openmrs/module/chartsearch/GeneratingJson.java
+++ api/src/main/java/org/openmrs/module/chartsearch/GeneratingJson.java
@@ -12,15 +12,18 @@ public class GeneratingJson {
         SearchAPI searchAPI =SearchAPI.getInstance();
         JSONObject jsonToReturn = new JSONObject();  //returning this object
         JSONArray arr_of_obs = new JSONArray();
+        JSONObject obs ;
         for(ChartListItem item : searchAPI.getResults()){ //foreach item from the search we populate the json
-            arr_of_obs.add(item.getObsDate());
-            arr_of_obs.add(item.getConceptName());
-            arr_of_obs.add(item.getValue());
-            arr_of_obs.add(item.getLocation());
-            //TODO add locations, add not only observations.
-            jsonToReturn.put("observations", arr_of_obs); //add the array to the json
+            obs = new JSONObject();
 
+            obs.put("date",item.getObsDate());
+            obs.put("concept name", item.getConceptName());
+            obs.put("value", item.getValue());
+            obs.put("location", item.getLocation());
+            //TODO add locations, add not only observations. 
+            arr_of_obs.add(obs);
         }
+        jsonToReturn.put("observations", arr_of_obs); //add the array to the json
 
         return jsonToReturn.toString();
     }

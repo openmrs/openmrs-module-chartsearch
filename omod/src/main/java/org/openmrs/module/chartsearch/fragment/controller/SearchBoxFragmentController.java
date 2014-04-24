2eaad37e802d8e029f81a93653db717b1924c9b0
orhemi <orhemi@walla.com>
2014-04-24 11:39:50 +0300

    fixing merge conflicts

--- omod/src/main/java/org/openmrs/module/chartsearch/fragment/controller/SearchBoxFragmentController.java
+++ omod/src/main/java/org/openmrs/module/chartsearch/fragment/controller/SearchBoxFragmentController.java
@@ -13,34 +13,48 @@ import org.openmrs.module.chartsearch.ObsItem;
 import org.openmrs.module.chartsearch.SearchAPI;
 import org.openmrs.module.chartsearch.SearchPhrase;
 import org.openmrs.module.chartsearch.solr.ChartSearchSearcher;
+<<<<<<< HEAD
 import org.openmrs.module.chartsearch.synonyms.SynonymGroup;
 import org.openmrs.module.chartsearch.synonyms.SynonymGroups;
 import org.openmrs.module.chartsearch.web.dwr.DWRChartSearchService;
+=======
+>>>>>>> fixing merge conflicts
 import org.openmrs.ui.framework.annotation.BindParams;
 import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
 
+<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+=======
+>>>>>>> fixing merge conflicts
 public class SearchBoxFragmentController {
     private ChartSearchSearcher searcher = getComponent(ChartSearchSearcher.class);
 
 	public void get() {
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	public String post(PageModel model, @BindParams SearchPhrase searchPhrase ,@RequestParam("patientId") Integer patient) {
 
         SearchAPI searchAPI =SearchAPI.getInstance();
         model.addAttribute("patientID_from_get", patient);
+<<<<<<< HEAD
 
         Integer length = Integer.valueOf(999999999);
+=======
+		//SearchAPI.setSearchPhrase(searchPhrase);
+		//SearchAPI.search();
+		//List<String> temp = new ArrayList<String>();
+		//temp.add(searchPhrase.getPhrase());
+        Integer length = Integer.valueOf(10);
+>>>>>>> fixing merge conflicts
         Integer start = Integer.valueOf(0);
 
         List<ChartListItem> items = new ArrayList<ChartListItem>();
@@ -65,14 +79,8 @@ public class SearchBoxFragmentController {
             items.add(itemsIsNull);
 
         }
-        List<ChartListItem> updatedItems = new ArrayList<ChartListItem>();
 
-        for(ChartListItem observation : items){
-            ChartListItem updatedObservation = DWRChartSearchService.getObservationDetails(observation.getObsId());
-            updatedItems.add(updatedObservation);
-        }
-
-        searchAPI.setResults(updatedItems);
+        searchAPI.setResults(items);
 
 
 		return "redirect:chartsearch/chartsearch.page?patientId=" + patient;
@@ -85,9 +93,9 @@ public class SearchBoxFragmentController {
             throw new RuntimeException("Cannot find component of " + clazz);
         return list.get(0);
     }
-	
-	
-	
-	
-	
+
+
+
+
+
 }
